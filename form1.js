b8b1fe1e4d20fa936c22ff2e0807536ad249c584
Neha-606 <nehashingade1004@gmail.com>
2025-11-15 19:07:25 +0530

    Add files via upload

---
+++ form1.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const loginLink = document.querySelector('.signInLink');
+const registerLink = document.querySelector('.signUpLink');
+
+registerLink.addEventListener('click',() =>{
+    container.classList.add('active');
+})
+
+loginLink.addEventListener('click',() =>{
+    container.classList.remove('active');
+})
